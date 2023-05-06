35c7a2f8fae9c657402b79bbe22956a0de50d30a
Salma Hamed <ihabsalma12@yahoo.com>
2023-05-06 13:59:59 +0300

    pda input

---
+++ CFGtoPDA/src/Main.java
@@ -0,0 +1,21 @@
+import com.sun.security.auth.NTSidUserPrincipal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+//    public static void main(String[] args) {
+//        App app = new App();
+//        try {
+//            app.ReceiveCFGInput(); //user input
+//            app.ProcessCFGInput(); //CFG processed to CNF then GNF
+//            app.CreatePDA(); //GNF converted to PDA
+//            app.DrawPDA(); //PDA Visualization
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+}
