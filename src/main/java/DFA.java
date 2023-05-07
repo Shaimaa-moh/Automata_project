7bf9c1518e07be6cc6c06476431f1c3dfaa4a8bb
Aya-gamal2211 <ayaahmadgamal2000@gmail.com>
2023-05-08 01:22:35 +0300

    NFA Transition Table
    Co-Authored-By: Shaimaa-moh <93445956+Shaimaa-moh@users.noreply.github.com>
    Co-Authored-By: ranawalid <103863926+ranawalid@users.noreply.github.com>

--- src/main/java/DFA.java
+++ src/main/java/DFA.java
@@ -9,21 +9,18 @@ public class DFA {
     static Group non_final_states; //group of non-final states
 
     public static void input(String file) throws IOException {
-    BufferedReader scan = null;
+        BufferedReader scan = null;
         try {
-        scan = new BufferedReader(new FileReader(file));
-    }
-        catch (FileNotFoundException e) {
-        e.printStackTrace();
-    }
-    int n = Integer.parseInt(scan.readLine());
-    dfa =new State[n];
-    for (int i = 0; i<n ; i++)
-
-    {
-        dfa[i] = new State(i);
+            scan = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int n = Integer.parseInt(scan.readLine());
+        dfa = new State[n];
+        for (int i = 0; i < n; i++) {
+            dfa[i] = new State(i);
 //        System.out.println(dfa[i]);
-    }
+        }
         String temp;
         inputSymbols = new HashSet<>();
 
@@ -52,14 +49,43 @@ public class DFA {
         }
 
 
-}
+    }
+    public static class NFATransitionTable {
+
+        private Map<Integer, Map<String, Set<Integer>>> table;
+
+        public NFATransitionTable() {
+            table = new HashMap<>();
+        }
+
+        public void addTransition(int state, String symbol, int nextState) {
+            Map<String, Set<Integer>> transitions = table.computeIfAbsent(state, k -> new HashMap<>());
+            Set<Integer> nextStates = transitions.computeIfAbsent(symbol, k -> new HashSet<>());
+            nextStates.add(nextState);
+        }
+
+        public Set<Integer> getNextStates(int state, char symbol) {
+            Map<String, Set<Integer>> transitions = table.get(state);
+            if (transitions == null) {
+                return null;
+            }
+            Set<Integer> nextStates = transitions.get(symbol);
+            return (nextStates != null) ? nextStates : new HashSet<Integer>();
+        }
+
+        public Set<Integer> getStates() {
+            return table.keySet();
+        }
+
+    }
+
 
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1 - NFA to DFA");
-        System.out.println(dfa);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("1 - NFA to DFA");
+//        System.out.println(dfa);
 //        System.out.println("2 - Minimize DFA");
 //
 //        int choice = sc.nextInt();
@@ -68,7 +94,20 @@ public class DFA {
 //            NFA_to_DFA.nfa_to_dfa();
 //        } else {
 //            DFA_Minimizer.minimize();
+        NFATransitionTable table = new NFATransitionTable();
+        table.addTransition(0, "a", 1);
+        table.addTransition(0, "b", 0);
+        table.addTransition(1, "a", 1);
+        table.addTransition(1, "b", 2);
+        table.addTransition(2, "a", 2);
+        table.addTransition(2, "b", 2);
+        for(Map.Entry<Integer, Map<String, Set<Integer>>> set:table.table.entrySet()){
+            System.out.print(set.getKey()+ " " + set.getValue());
+
+        }
 
 
     }
+
+
 }
--- src/main/java/NFA.java
+++ src/main/java/NFA.java
@@ -1,22 +1,55 @@
-import java.io.*;
 import java.util.*;
-import java. util. Scanner;
+import java.lang.*;
 
 public class NFA {
-    int [] states;
+    int[] states;
     char[] alphabets;
-    int[] transition;
+    int[][] transition;
     int start;
     int[] Final;
-    Map<String, Integer> Transition_Table = new HashMap<String, Integer>();
-    public NFA(int [] states,char[] alphabets, int[] transition, int start, int[] Final){
-        this.states=states;
-        this.alphabets=alphabets;
-        this.transition=transition;
-        this.start=start;
-        this.Final=Final;
+    HashMap<String, String> Transition_Table = new HashMap<String, String>();
+
+    HashMap<Integer, Integer> No_States = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> No_Alphabets = new HashMap<Integer, Integer>();
+
+    public NFA(int[] states, char[] alphabets, int[][] transition, int start, int[] Final) {
+        this.states = states;
+        this.alphabets = alphabets;
+        this.transition = transition;
+        this.start = start;
+        this.Final = Final;
 
     }
-
-//    public
+    
 }
+
+//    public void convert(){
+//        for(int i=0;i<states.length;i++ ){
+//            No_States.put(states[i],i);
+//        }
+//        for(int i=0;i<alphabets.length;i++ ){
+//            No_Alphabets.put((int) alphabets[i],i);
+//        }
+//        for(int i=0;i<states.length;i++ ){
+//            for(int j=0;j<alphabets.length;j++ ) {
+//                Transition_Table.put(Integer.toString(i), Integer.toString(j));
+//            }
+//        }
+//        for(int k=0; k<transition.length;k++){
+//            Transition_Table.get(No_States.get(transition[k][0])).concat(String.valueOf(No_Alphabets.get(transition[k][1]))).append(No_States.get(transition[k][2]));
+//
+//        }
+//
+//
+//
+////        for i in range(self.no_state):
+////        for j in range(self.no_alphabet):
+////        self.transition_table[str(i) + str(j)] = []
+////        for i in range(self.no_transition):
+////        self.transition_table[str(self.states_dict[self.transitions[i][0]])
+////                + str(self.alphabets_dict[
+////                self.transitions[i][1]])].append(
+////                self.states_dict[self.transitions[i][2]])
+//
+//    }
+//}
